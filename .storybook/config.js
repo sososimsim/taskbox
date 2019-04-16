c0a84191f9220ba6f7dd15ff1b4170403fa1207f
sososimsim <phewphewsimsim@naver.com>
2019-04-17 02:14:19 +0900

    sample : Task Component, snapshot testing까지 추가한 예제 코드

---
+++ .storybook/config.js
@@ -0,0 +1,11 @@
+import { configure } from '@storybook/react';
+import '../src/index.css';
+import requireContext from 'require-context.macro';
+
+const req = requireContext('../src', true, /.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
